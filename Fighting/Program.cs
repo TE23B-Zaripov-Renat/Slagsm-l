8fbb53e432f0bc44dab750dcd53877e6c095c241
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-10-02 14:53:49 +0200

    oibnhjhibljhib

--- Fighting/Program.cs
+++ Fighting/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+
+int hp = Random.Shared.Next(50, 101);
+Console.WriteLine(hp);
+Console.ReadLine();
+}
+
+
+Console.WriteLine("Skriv ditt namn");
+string name = Console.ReadLine();
+
+while (name == "")
+{
+    Console.WriteLine("Du kan ju inte bara trycka enter, dumhuvve");
+    Console.WriteLine("Försök igen:");
+    name = Console.ReadLine();
+}
+
+
+
+Console.ReadLine();
